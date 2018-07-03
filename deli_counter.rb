9e4088a0eb82a848c760a43b9563940e6433e3d3
Jessica Long <jessicalong627@gmail.com>
2018-07-03 20:38:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,11 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else 
-    puts "The line is currently: #{katz_deli}"
+    message="The line is currently:"
+    katz_deli.each_with_index do |value, index|
+    message += " #{index.to_i+1}. #{value}"
   end
+  puts message
 end
 
 def take_a_number(katz_deli, name)

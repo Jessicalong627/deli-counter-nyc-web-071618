59a36f060aa37cb21593f8598d868b1d0b503b3e
Jessica Long <jessicalong627@gmail.com>
2018-07-03 20:24:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-puts "Welcome, #{name}."
+puts "Welcome, #{name}. You are number #{kat_deli.length} in line."
 end
 
 def now_serving(katz_deli)
